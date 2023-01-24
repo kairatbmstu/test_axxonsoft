9331fa1f826beda021b7d1e6f973604d9b722fd1
Юсупов Кайрат <k.yussupov@infin-it.kz>
2023-01-24 22:12:05 +0600

    Added schema for cassandra;

--- migrations/001_task_table.up.sql
+++ migrations/001_task_table.up.sql
@@ -1,10 +1,11 @@
-CREATE TABLE task (
-	id uuid not null PRIMARY KEY,
-	method varchar(10) not null,
-	url varchar(2048) NULL,
-	http_status_code int not null,
-	task_status varchar(50),
-	response_length int not null,
+create table task (
+	id UUID, 
+	method varchar,
+	url varchar,
+	headers map<varchar, varchar>,
+	http_status_code int,
+	task_status varchar,
+	response_length int,
 	request_body text,
-	response_body text
-);
+	response_body text,
+PRIMARY KEY(id));

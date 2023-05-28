c28585944f645d116b0fd13e180da35091c24424
Kairat Yussupov <k.yussupov@infin-it.kz>
2023-05-28 23:08:56 +0600

    Added documentation;

--- dto/dto.go
+++ dto/dto.go
@@ -29,6 +29,25 @@ type TaskDTO struct {
 	ResponseBody    string            `json:"responseBody"`
 }
 
+/*
+The CreateTaskDTO type represents the data transfer object (DTO) structure used for creating a task. It contains the following fields:
+
+Method (string): The HTTP method for the task. It is a required field and is tagged with binding:"required",
+indicating that it must be provided in the input data. This field specifies the HTTP method such as "GET", "POST", "PUT", etc.
+
+Url (string): The URL for the task. It is a required field and is tagged with binding:"required", indicating
+that it must be provided in the input data. This field represents the URL to which the HTTP request should be sent.
+
+HttpStatusCode (int): The HTTP status code for the task. This field holds the desired HTTP status code
+that the task expects in the response.
+
+RequestHeaders (map[string]string): The headers to be included in the request. This field is a map
+where the keys represent the header names, and the values represent the corresponding header values.
+It allows specifying additional headers for the task.
+
+RequestBody (string): The body of the request. This field holds the body content that should be
+included in the HTTP request.
+*/
 type CreateTaskDTO struct {
 	Method         string            `json:"method" binding:"required"`
 	Url            string            `json:"url" binding:"required"`
